22ab402f6d3ff1c4de0dc5a3d980837f8fc6ebe4
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2023-11-09 10:38:13 +0800

    updates

--- SECOND-YEAR-PROGRAMMING-PROJECTS/First Semester Assignments/JAVA-Assignments/Euclidean's Algorithm/MainTest.java
+++ SECOND-YEAR-PROGRAMMING-PROJECTS/First Semester Assignments/JAVA-Assignments/Euclidean's Algorithm/MainTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 public class MainTest {
-    int testCaseNumber = 1;
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_RESET = "\u001B[0m";
 
@@ -10,38 +8,34 @@ public class MainTest {
     public void testEuclideanGCD1() {
         long result = Main.euclideanGCD(10, 5);
         assertEquals(5, result);
-        System.out.println(ANSI_GREEN + "Test Case #" + testCaseNumber + ": Passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Test Case #" + 1 + ": Passed" + ANSI_RESET);
     }
 
     @Test
     public void testEuclideanGCD2() {
         long result = Main.euclideanGCD(608, 133);
         assertEquals(19, result);
-        testCaseNumber++;
-        System.out.println(ANSI_GREEN + "Test Case #" + testCaseNumber + ": Passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Test Case #" + 2+ ": Passed" + ANSI_RESET);
     }
 
     @Test
     public void testEuclideanGCD3() {
         long result = Main.euclideanGCD(9, 6);
         assertEquals(3, result);
-        testCaseNumber++;
-        System.out.println(ANSI_GREEN + "Test Case #" + testCaseNumber + ": Passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Test Case #" + 3 + ": Passed" + ANSI_RESET);
     }
 
     @Test
     public void testEuclideanGCD4() {
         long result = Main.euclideanGCD(17, 8);
         assertEquals(1, result);
-        testCaseNumber++;
-        System.out.println(ANSI_GREEN + "Test Case #" + testCaseNumber + ": Passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Test Case #" + 4  + ": Passed" + ANSI_RESET);
     }
 
     @Test
     public void testEuclideanGCD5() {
         long result = Main.euclideanGCD(14, 12);
         assertEquals(2, result);
-        testCaseNumber++;
-        System.out.println(ANSI_GREEN + "Test Case #" + testCaseNumber + ": Passed" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Test Case #" + 5 + ": Passed" + ANSI_RESET);
     }
 }
